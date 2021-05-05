02f874448d804c63122e113935f4f9cf6e54bbbf
AdityaPutraS <adityaputra159@gmail.com>
2021-05-05 09:58:45 +0800

    Add best model & update demo program

--- env_vis_hier.py
+++ env_vis_hier.py
@@ -43,120 +43,121 @@ if __name__ == "__main__":
     ray.shutdown()
     ray.init(ignore_reinit_error=True)
 
-    env = HierarchicalHumanoidEnv()
-    env.max_timestep = 100000
-    env.usePredefinedTarget = True
 
-    agent = PPOTrainer(config_hier)
-    experiment_name = "HWalk_Hier_Mimic"
+    model = input("Jenis model (08_03, 09_03, quit): ").lower()
+    while(model != 'quit'):
 
-    model = input("Jenis model (08_03, 09_03): ").lower()
-    # experiment_id = "train_HumanoidBulletEnvHier-v0_ddce5_00000_0_2021-04-27_20-12-42"
-    # checkpoint_num = "1660"
+        env = HierarchicalHumanoidEnv()
+        env.max_timestep = 100000
+        env.usePredefinedTarget = True
 
-    if(model == "08_03"):
-        # Motion 08_03
-        experiment_id = "train_HumanoidBulletEnvHier-v0_88b64_00000_0_2021-05-02_12-26-15"
-        checkpoint_num = "960"
-        env.selected_motion = 0
-        print("Selesai load model motion 08_03")
-    else:
-        # Motion 09_03
-        experiment_id = "train_HumanoidBulletEnvHier-v0_0cbfa_00000_0_2021-05-01_17-03-09"
-        checkpoint_num = "1320"
-        env.selected_motion = 1
-        print("Selesai load model motion 09_03")
+        agent = PPOTrainer(config_hier)
+        experiment_name = "HWalk_Hier_Mimic"
+        
+        if(model == "08_03"):
+            # Motion 08_03
+            experiment_id = "train_HumanoidBulletEnvHier-v0_88b64_00000_0_2021-05-02_12-26-15"
+            checkpoint_num = "960"
+            env.selected_motion = 0
+            print("Selesai load model motion 08_03")
+        else:
+            # Motion 09_03
+            experiment_id = "train_HumanoidBulletEnvHier-v0_0cbfa_00000_0_2021-05-01_17-03-09"
+            checkpoint_num = "1320"
+            env.selected_motion = 1
+            print("Selesai load model motion 09_03")
 
-    agent.restore(
-        "/home/aditya/ray_results/{}/{}/checkpoint_{}/checkpoint_{}/checkpoint-{}".format(
-            experiment_name, experiment_id, checkpoint_num, checkpoint_num, checkpoint_num
+        agent.restore(
+            "/home/aditya/ray_results/{}/{}/checkpoint_{}/checkpoint_{}/checkpoint-{}".format(
+                experiment_name, experiment_id, checkpoint_num, checkpoint_num, checkpoint_num
+            )
         )
-    )
 
-    tipeTarget = input("Jenis target (kotak, m, segi_enam, 180): ").lower()
-    if (tipeTarget == 'kotak'):
-        # Kotak
-        env.predefinedTarget = np.array([
-            [5, 0, 0],
-            [5, -5, 0],
-            [0, -5, 0],
-            [0, 0, 0]
-        ])
-    elif (tipeTarget == 'm'):
-        # M
-        env.predefinedTarget = np.array([
-            [5, -1, 0],
-            [0, -2, 0],
-            [5, -3, 0],
-            [0, -4, 0],
-            [0, 0, 0]
-        ])
-    elif (tipeTarget == 'segi enam'):
-        # Segi Enam
-        env.predefinedTarget = np.array([
-            [0, 5, 0],
-            [4.33, 2.5, 0],
-            [4.33, -2.5, 0],
-            [0, -5, 0],
-            [-4.33, -2.5, 0],
-            [-4.33, 2.5, 0],
-        ])
-    elif (tipeTarget == '180'):
-        # Putar 180 Derajat
-        env.predefinedTarget = np.array([
-            [0, 5, 0],
-            [0, -5, 0],
-            [-5, 0, 0],
-            [5, 0, 0]
-        ])
+        tipeTarget = input("Jenis target (kotak, m, segi_enam, 180): ").lower()
+        if (tipeTarget == 'kotak'):
+            # Kotak
+            env.predefinedTarget = np.array([
+                [5, 0, 0],
+                [5, -5, 0],
+                [0, -5, 0],
+                [0, 0, 0]
+            ])
+        elif (tipeTarget == 'm'):
+            # M
+            env.predefinedTarget = np.array([
+                [5, -1, 0],
+                [0, -2, 0],
+                [5, -3, 0],
+                [0, -4, 0],
+                [0, 0, 0]
+            ])
+        elif (tipeTarget == 'segi enam'):
+            # Segi Enam
+            env.predefinedTarget = np.array([
+                [0, 5, 0],
+                [4.33, 2.5, 0],
+                [4.33, -2.5, 0],
+                [0, -5, 0],
+                [-4.33, -2.5, 0],
+                [-4.33, 2.5, 0],
+            ])
+        elif (tipeTarget == '180'):
+            # Putar 180 Derajat
+            env.predefinedTarget = np.array([
+                [0, 5, 0],
+                [0, -5, 0],
+                [-5, 0, 0],
+                [5, 0, 0]
+            ])
 
-    fps = 240.0
-    qKey = ord("q")
-    rKey = ord("r")
-    eKey = ord("e")
+        fps = 240.0
+        qKey = ord("q")
+        rKey = ord("r")
+        eKey = ord("e")
 
-    doneAll = False
-    logStarted = False
-    while not doneAll:
-        done = False
-        env.render()
-        observation = env.resetFromFrame(startFrame=50)
-        drawAxis()
-        if(not(logStarted)):
-            pybullet.startStateLogging(pybullet.STATE_LOGGING_VIDEO_MP4, "./out/video/hier_{}_{}_{}_{}.mp4".format(experiment_id[-19:], checkpoint_num, model, tipeTarget))
-            logStarted = True
-        pause = True
-        while not done and not doneAll:
-            action = dict()
-            if(not pause):
-                if('high_level_agent' in observation):
-                    action['high_level_agent'] = agent.compute_action(observation['high_level_agent'], policy_id='high_level_policy')
-                    # if(not pause):
-                        # pause = True
-                else:
-                    action[env.low_level_agent_id] = agent.compute_action(observation[env.low_level_agent_id], policy_id='low_level_policy')
-                observation, reward, f_done, info = env.step(action, debug=False)
-                # done = f_done['__all__'] == True
-                # if(f_done['__all__']):
-                    # print("Done")
-            targetHL = np.array([
-                np.cos(env.highLevelDegTarget),
-                np.sin(env.highLevelDegTarget),
-                0
-            ]) * 5
-            drawLine(env.robot_pos, env.robot_pos + targetHL, [0, 1, 0])
+        doneAll = False
+        logStarted = False
+        while not doneAll:
+            done = False
+            env.render()
+            observation = env.resetFromFrame(startFrame=50)
+            drawAxis()
+            if(not(logStarted)):
+                pybullet.startStateLogging(pybullet.STATE_LOGGING_VIDEO_MP4, "./out/video/hier_{}_{}_{}_{}.mp4".format(experiment_id[-19:], checkpoint_num, model, tipeTarget))
+                logStarted = True
+            pause = True
+            while not done and not doneAll:
+                action = dict()
+                if(not pause):
+                    if('high_level_agent' in observation):
+                        action['high_level_agent'] = agent.compute_action(observation['high_level_agent'], policy_id='high_level_policy')
+                        # if(not pause):
+                            # pause = True
+                    else:
+                        action[env.low_level_agent_id] = agent.compute_action(observation[env.low_level_agent_id], policy_id='low_level_policy')
+                    observation, reward, f_done, info = env.step(action, debug=False)
+                    # done = f_done['__all__'] == True
+                    # if(f_done['__all__']):
+                        # print("Done")
+                targetHL = np.array([
+                    np.cos(env.highLevelDegTarget),
+                    np.sin(env.highLevelDegTarget),
+                    0
+                ]) * 5
+                drawLine(env.robot_pos, env.robot_pos + targetHL, [0, 1, 0])
 
-            # time.sleep(1.0 / fps)
+                # time.sleep(1.0 / fps)
 
-            keys = pybullet.getKeyboardEvents()
-            if qKey in keys and keys[qKey] & pybullet.KEY_WAS_TRIGGERED:
-                print("QUIT")
-                doneAll = True
-            elif rKey in keys and keys[rKey] & pybullet.KEY_WAS_TRIGGERED:
-                done = True
-            elif eKey in keys and keys[eKey] & pybullet.KEY_WAS_TRIGGERED:
-                pause = not pause
-        print("Hidup selama {} steps".format(env.cur_timestep))
-        pybullet.removeAllUserDebugItems()
-    env.close()
+                keys = pybullet.getKeyboardEvents()
+                if qKey in keys and keys[qKey] & pybullet.KEY_WAS_TRIGGERED:
+                    print("QUIT")
+                    doneAll = True
+                elif rKey in keys and keys[rKey] & pybullet.KEY_WAS_TRIGGERED:
+                    done = True
+                elif eKey in keys and keys[eKey] & pybullet.KEY_WAS_TRIGGERED:
+                    pause = not pause
+            print("Hidup selama {} steps".format(env.cur_timestep))
+            pybullet.removeAllUserDebugItems()
+        env.close()
+        model = input("Jenis model (08_03, 09_03, quit): ").lower()
     ray.shutdown()
--- env_vis_low.py
+++ env_vis_low.py
@@ -50,157 +50,163 @@ if __name__ == "__main__":
     # Khusus 19-33-42 (2580)
     # config_low["model"]["fcnet_hiddens"] = [256, 128]
 
-    agent = PPOTrainer(config_low)
-    experiment_name = "HWalk_Low_Mimic"
 
-    model = input("Jenis model (08_03, 09_03, custom_env): ").lower()
-    useCustomEnv = False
-    # Tanpa endpoint reward
-    if(model == '08_03'):
-        # Motion 08_03
-        experiment_id = "PPO_HumanoidBulletEnv-v0-Low_68eec_00000_0_2021-05-01_23-32-16"
-        checkpoint_num = "1610"
-        motion_used = "motion08_03"
-        print("Selesai load model motion 08_03")
-    elif(model == 'custom_env'):
-        # Custom env
-        experiment_id = "PPO_HumanoidBulletEnv-v0-Low_54134_00000_0_2021-05-03_21-13-19"
-        checkpoint_num = "3630"
-        motion_used = "motion09_03"
-        useCustomEnv = True
-        print("Selesai load model custom env motion 09_03")
-    else:
-        # Motion 09_03
-        experiment_id = "PPO_HumanoidBulletEnv-v0-Low_6d114_00000_0_2021-04-30_23-26-25"
-        checkpoint_num = "1690"
-        motion_used = "motion09_03"
-        print("Selesai load model motion 09_03")
-
+    model = input("Jenis model (08_03, 09_03, custom_env, quit): ").lower()
+    while(model != 'quit'):
     
+        agent = PPOTrainer(config_low)
+        experiment_name = "HWalk_Low_Mimic"
+        
+        useCustomEnv = False
+        # Tanpa endpoint reward
+        if(model == '08_03'):
+            # Motion 08_03
+            experiment_id = "PPO_HumanoidBulletEnv-v0-Low_68eec_00000_0_2021-05-01_23-32-16"
+            checkpoint_num = "1610"
+            motion_used = "motion08_03"
+            print("Selesai load model motion 08_03")
+        elif(model == 'custom_env'):
+            # Custom env
+            experiment_id = "PPO_HumanoidBulletEnv-v0-Low_54134_00000_0_2021-05-03_21-13-19"
+            checkpoint_num = "3640"
+            motion_used = "motion09_03"
+            useCustomEnv = True
+            print("Selesai load model custom env motion 09_03")
+        else:
+            # Motion 09_03
+            experiment_id = "PPO_HumanoidBulletEnv-v0-Low_6d114_00000_0_2021-04-30_23-26-25"
+            checkpoint_num = "1690"
+            motion_used = "motion09_03"
+            print("Selesai load model motion 09_03")
 
-    agent.restore(
-        "/home/aditya/ray_results/{}/{}/checkpoint_{}/checkpoint-{}".format(
-            experiment_name, experiment_id, checkpoint_num, checkpoint_num
-        )
-    )
+        
 
-    # 29_21-30-25 (motion08_03)
-    # 25_19-33-42 (motion09_03)
-
-    env = LowLevelHumanoidEnv(reference_name=motion_used, useCustomEnv=useCustomEnv)
-    env.usePredefinedTarget = True
-    
-    tipeTarget = input("Jenis target (kotak, m, segi_enam, 180, lurus, tanjakan): ").lower()
-    if (tipeTarget == 'kotak'):
-        # Kotak
-        env.predefinedTarget = np.array([
-            [5, 0, 0],
-            [5, -5, 0],
-            [0, -5, 0],
-            [0, 0, 0]
-        ])
-    elif (tipeTarget == 'm'):
-        # M
-        env.predefinedTarget = np.array([
-            [5, -1, 0],
-            [0, -2, 0],
-            [5, -3, 0],
-            [0, -4, 0],
-            [0, 0, 0]
-        ])
-    elif (tipeTarget == 'segi enam'):
-        # Segi Enam
-        env.predefinedTarget = np.array([
-            [0, 5, 0],
-            [4.33, 2.5, 0],
-            [4.33, -2.5, 0],
-            [0, -5, 0],
-            [-4.33, -2.5, 0],
-            [-4.33, 2.5, 0],
-        ])
-    elif (tipeTarget == '180'):
-        # Putar 180 Derajat
-        env.predefinedTarget = np.array([
-            [0, 5, 0],
-            [0, -5, 0],
-            [-5, 0, 0],
-            [5, 0, 0]
-        ])
-    elif (tipeTarget == 'lurus' or tipeTarget == 'tanjakan'):
-        # Lurus 10 meter, kemudian balik
-        env.predefinedTarget = np.array([
-            [10, 0, 0],
-            [0, 0, 0]
-        ])
-
-    fps = 240.0
-    qKey = ord("q")
-    rKey = ord("r")
-    eKey = ord("e")
-
-    doneAll = False
-    logStarted = False
-    
-    # Buat tanjakan untuk custom env
-    if(useCustomEnv and tipeTarget == 'tanjakan'):
-        terrainData = [0] * 256 * 256
-        for j in range(63-5, 64+5+1):
-            for i in range(63, 68):
-                terrainData[2 * i + 2 * j * 256] = (i-63)/10
-                terrainData[2 * i + 1 + 2 * j * 256] = (i-63)/10
-                terrainData[2 * i + (2 * j + 1) * 256] = (i-63)/10
-                terrainData[2 * i + 1 + (2 * j + 1) * 256] = (i-63)/10
-
-    while not doneAll:
-        done = False
-        env.render()
-        observation = env.reset(startFrame=0, startFromRef=True)
-        if(useCustomEnv and tipeTarget == 'tanjakan'):
-            env.flat_env.stadium_scene.replaceHeightfieldData(terrainData)
-            print("Selesai replace heightfield")
-        drawAxis()
-        if(not(logStarted)):
-            pybullet.startStateLogging(pybullet.STATE_LOGGING_VIDEO_MP4, "./out/video/{}_{}_{}_{}.mp4".format(experiment_id[-19:], checkpoint_num, model, tipeTarget))
-            logStarted = True
-        pause = True
-
-        # drift = []
-        while not done and not doneAll:
-            if not pause:
-                action = agent.compute_action(observation)
-                observation, reward, f_done, info = env.step(action)
-                # done = f_done
-                # drift.append(calcDrift(env.robot_pos, env.starting_robot_pos, env.target))
-            
-            drawLine(
-                env.robot_pos,
-                env.target,
-                [0, 0, 1],
+        agent.restore(
+            "/home/aditya/ray_results/{}/{}/checkpoint_{}/checkpoint-{}".format(
+                experiment_name, experiment_id, checkpoint_num, checkpoint_num
             )
+        )
 
-            # drawLine(
-            #     env.starting_ep_pos,
-            #     env.target,
-            #     [0, 0, 0],
-            # )
+        # 29_21-30-25 (motion08_03)
+        # 25_19-33-42 (motion09_03)
 
-            # drawLine(
-            #     env.starting_ep_pos,
-            #     env.robot_pos,
-            #     [1, 1, 1]
-            # )
+        env = LowLevelHumanoidEnv(reference_name=motion_used, useCustomEnv=useCustomEnv)
+        env.usePredefinedTarget = True
+        
+        tipeTarget = input("Jenis target (kotak, m, segi_enam, 180, lurus, tanjakan): ").lower()
+        if (tipeTarget == 'kotak'):
+            # Kotak
+            env.predefinedTarget = np.array([
+                [5, 0, 0],
+                [5, -5, 0],
+                [0, -5, 0],
+                [0, 0, 0]
+            ])
+        elif (tipeTarget == 'm'):
+            # M
+            env.predefinedTarget = np.array([
+                [5, -1, 0],
+                [0, -2, 0],
+                [5, -3, 0],
+                [0, -4, 0],
+                [0, 0, 0]
+            ])
+        elif (tipeTarget == 'segi enam'):
+            # Segi Enam
+            env.predefinedTarget = np.array([
+                [0, 5, 0],
+                [4.33, 2.5, 0],
+                [4.33, -2.5, 0],
+                [0, -5, 0],
+                [-4.33, -2.5, 0],
+                [-4.33, 2.5, 0],
+            ])
+        elif (tipeTarget == '180'):
+            # Putar 180 Derajat
+            env.predefinedTarget = np.array([
+                [0, 5, 0],
+                [0, -5, 0],
+                [-5, 0, 0],
+                [5, 0, 0]
+            ])
+        elif (tipeTarget == 'lurus' or tipeTarget == 'tanjakan'):
+            # Lurus 10 meter, kemudian balik
+            env.predefinedTarget = np.array([
+                [10, 0, 0],
+                [0, 0, 0]
+            ])
 
-            # time.sleep(1.0 / fps)
+        fps = 240.0
+        qKey = ord("q")
+        rKey = ord("r")
+        eKey = ord("e")
 
-            keys = pybullet.getKeyboardEvents()
-            if qKey in keys and keys[qKey] & pybullet.KEY_WAS_TRIGGERED:
-                print("QUIT")
-                doneAll = True
-            elif rKey in keys and keys[rKey] & pybullet.KEY_WAS_TRIGGERED:
-                done = True
-            elif eKey in keys and keys[eKey] & pybullet.KEY_WAS_TRIGGERED:
-                pause = not pause
-        print("    Hidup selama {} steps".format(env.cur_timestep))
-        pybullet.removeAllUserDebugItems()
-    env.close()
+        doneAll = False
+        logStarted = False
+        
+        # Buat tanjakan untuk custom env
+        if(useCustomEnv and tipeTarget == 'tanjakan'):
+            terrainData = [0] * 256 * 256
+            for j in range(63-5, 64+5+1):
+                for i in range(63, 68):
+                    terrainData[2 * i + 2 * j * 256] = (i-63)/10
+                    terrainData[2 * i + 1 + 2 * j * 256] = (i-63)/10
+                    terrainData[2 * i + (2 * j + 1) * 256] = (i-63)/10
+                    terrainData[2 * i + 1 + (2 * j + 1) * 256] = (i-63)/10
+
+        while not doneAll:
+            done = False
+            env.render()
+            observation = env.reset(startFrame=0, startFromRef=True)
+            if(useCustomEnv and tipeTarget == 'tanjakan'):
+                env.flat_env.stadium_scene.replaceHeightfieldData(terrainData)
+                print("Selesai replace heightfield")
+            drawAxis()
+            if(not(logStarted)):
+                pybullet.startStateLogging(pybullet.STATE_LOGGING_VIDEO_MP4, "./out/video/{}_{}_{}_{}.mp4".format(experiment_id[-19:], checkpoint_num, model, tipeTarget))
+                logStarted = True
+            pause = True
+
+            # drift = []
+            while not done and not doneAll:
+                if not pause:
+                    action = agent.compute_action(observation)
+                    observation, reward, f_done, info = env.step(action)
+                    # done = f_done
+                    # drift.append(calcDrift(env.robot_pos, env.starting_robot_pos, env.target))
+                
+                drawLine(
+                    env.robot_pos,
+                    env.target,
+                    [0, 0, 1],
+                )
+
+                # drawLine(
+                #     env.starting_ep_pos,
+                #     env.target,
+                #     [0, 0, 0],
+                # )
+
+                # drawLine(
+                #     env.starting_ep_pos,
+                #     env.robot_pos,
+                #     [1, 1, 1]
+                # )
+
+                # time.sleep(1.0 / fps)
+
+                keys = pybullet.getKeyboardEvents()
+                if qKey in keys and keys[qKey] & pybullet.KEY_WAS_TRIGGERED:
+                    print("QUIT")
+                    doneAll = True
+                elif rKey in keys and keys[rKey] & pybullet.KEY_WAS_TRIGGERED:
+                    done = True
+                elif eKey in keys and keys[eKey] & pybullet.KEY_WAS_TRIGGERED:
+                    pause = not pause
+            print("    Hidup selama {} steps".format(env.cur_timestep))
+            pybullet.removeAllUserDebugItems()
+        env.close()
+    
+        model = input("Jenis model (08_03, 09_03, custom_env, quit): ").lower()
+
     ray.shutdown()
